7b7e175d965847b7d38aaaa427ab979b59c5c9c3
Nathaniel Millard <nathanielmillard@gmail.com>
2021-02-17 11:02:06 -0700

    Build basic idea card

--- src/Components/IdeaCard/IdeaCard.js
+++ src/Components/IdeaCard/IdeaCard.js
@@ -1,11 +1,26 @@
 import './IdeaCard.css';
+import PropTypes from 'prop-types'
 
 function IdeaCard() {
   return (
-    <div className="IdeaCard">
-
-    </div>
+    <article className="IdeaCard">
+    <header>
+      <button><img src={} /></button>
+      <button><img src={} /></button>
+    </header>
+      <h1>{this.props.title}</h1>
+      <p>{this.props.body}</p>
+    <footer>
+      <button><img src={} /></button>
+      Comment
+    </footer>
+    </article>
   );
 }
 
 export default IdeaCard;
+
+IdeaCard.propTypes = {
+  title = PropTypes.string.isRequired,
+  body = PropTypes.string.isRequired,
+}
