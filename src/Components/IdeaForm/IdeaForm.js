64fdbff5f168eb0d9ba4e54bbbc03729b2a04cdc
Nathaniel Millard <nathanielmillard@gmail.com>
2021-02-17 10:01:06 -0700

    Create class based IdeaForm component.

--- src/Components/IdeaForm/IdeaForm.js
+++ src/Components/IdeaForm/IdeaForm.js
@@ -1,11 +1,37 @@
 import './IdeaForm.css';
+import {Component} from 'react'
 
-function IdeaForm() {
-  return (
-    <div className="IdeaForm">
-
-    </div>
-  );
+class IdeaForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      this.title = '',
+      this.body = ''
+    }
+  }
+  render(){
+    return (
+      <div className="IdeaForm">
+        <form className='ideaForm'>
+          <label>
+            Title
+            <input type='text'/>
+          </label>
+          <label>
+            Body
+            <textarea />
+          </label>
+          <button className='saveButton'> Save </button>
+        </form>
+        <form className='searchForm'>
+          <input />
+          <button className='searchButton'>
+            <img />
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default IdeaForm;
