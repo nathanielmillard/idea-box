5426f7a9d511f318c37523154effaed9c12e7bf1
Nathaniel Millard <nathanielmillard@gmail.com>
2021-02-17 09:49:48 -0700

    Build generall App structure.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import ControlBar from '../ControlBar/ControlBar.js'
+import IdeaForm from '../IdeaForm/IdeaForm.js'
+import CardArea from '../CardArea/CardArea.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ControlBar />
+      <IdeaForm />
+      <CardArea />
     </div>
   );
 }
