fdf5f5bf67fd307536689ad988981859e4b46534
Nathaniel Millard <nathanielmillard@gmail.com>
2021-02-18 10:39:39 -0700

    Built nav bar.

--- src/Components/ControlBar/ControlBar.js
+++ src/Components/ControlBar/ControlBar.js
@@ -2,9 +2,13 @@ import './ControlBar.css';
 
 function ControlBar() {
   return (
-    <div className="ControlBar">
-
-    </div>
+    <nav className="ControlBar">
+      <h1>IdeaBox</h1>
+      <div>
+        <h2>Filter Starred Ideas</h2>
+        <button> Show Starred Ideas </button>
+      </div>
+    </nav>
   );
 }
 
